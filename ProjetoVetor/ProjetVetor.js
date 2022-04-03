1b2e37c122cbf2c4b2d96510b6f9aeca2bba791c
Hugo <hugosantosadm125@gmail.com>
2022-04-03 17:37:42 -0300

    Novo Exercicio

---
+++ ProjetoVetor/ProjetVetor.js
@@ -0,0 +1,34 @@
+var alf = new Array();
+    alf[0] = "A";
+    alf[1] = "B";
+    alf[2] = "C";
+    alf[3] = "D";
+    alf[4] = "E";
+    alf[5] = "F";
+    alf[6] = "G";
+    alf[7] = "H";
+    alf[8] = "I";
+    alf[9] = "J";
+    alf[10] = "K";
+    alf[11] = "L";
+    alf[12] = "M";
+    alf[13] = "N";
+    alf[14] = "O";
+    alf[15] = "P";
+    alf[16] = "Q";
+    alf[17] = "R";
+    alf[18] = "S";
+    alf[19] = "T";
+    alf[20] = "U";
+    alf[21] = "V";
+    alf[22] = "W";
+    alf[23] = "X";
+    alf[24] = "Y";
+    alf[25] = "Z";
+
+    document.write(alf);
+    document.write("<br/>");
+    document.write("<br/>");
+    
+    var resultado = `${alf[7]}${alf[20]}${alf[6]}${alf[14]}  ${alf[5]}${alf[4]}${alf[17]}${alf[17]}${alf[4]}${alf[8]}${alf[17]}${alf[0]} ${alf[3]}${alf[14]}${alf[18]} ${alf[18]}${alf[0]}${alf[13]}${alf[19]}${alf[14]}${alf[18]}`
+    document.write(resultado);
